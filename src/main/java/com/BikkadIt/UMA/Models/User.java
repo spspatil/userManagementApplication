216370ae75982e72557cb23833127fe66253341c
shinusmp <shrinivaspatil12@gmail.com>
2022-06-22 23:46:20 +0530

    UMA-03:created requiredpckg

---
+++ src/main/java/com/BikkadIt/UMA/Models/User.java
@@ -0,0 +1,22 @@
+package com.BikkadIt.UMA.Models;
+
+public class User {
+	
+	
+	private Integer userId;
+	
+	private String userFirstName;
+	
+	private String userLastName;
+	
+	private String userEmail;
+	
+	private String userPhNo;
+	
+	private String userDOB;
+	
+	private String gender;
+	
+	
+
+}
