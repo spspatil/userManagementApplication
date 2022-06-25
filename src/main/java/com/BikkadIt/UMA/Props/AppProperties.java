c4fc216ff9e7ec6a015c67880cfcf0e81338e78f
shinusmp <shrinivaspatil12@gmail.com>
2022-06-25 15:05:29 +0530

    UMAP-11:created appproperties class

---
+++ src/main/java/com/BikkadIt/UMA/Props/AppProperties.java
@@ -0,0 +1,28 @@
+package com.BikkadIt.UMA.Props;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "UMAP1")
+public class AppProperties {
+	
+	private Map<String, String> messages=new HashMap<>();
+
+	public Map<String, String> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(Map<String, String> messages) {
+		this.messages = messages;
+	}
+	
+	
+
+}
+
