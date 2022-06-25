56c4ffa9999a301ed8b9edc2e094012be3824176
shinusmp <shrinivaspatil12@gmail.com>
2022-06-25 15:08:46 +0530

    UMAP-12:created Appconstants class

---
+++ src/main/java/com/BikkadIt/UMA/Props/AppConstants.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.UMA.Props;
+
+public class AppConstants {
+	
+	public static final String SAVE_SUCCESS="saveSuccess";
+	public static final String SAVE_FAIL="saveFail";
+	public static final String DELETED="Deleted";
+	public static final String NOT_DELETED="NotDeleted";
+	public static final char Y='Y';
+	public static final char N='N';
+
+
+}
