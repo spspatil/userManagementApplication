18393b1bb7a1405f1ef59acf531f5074eb98b34b
shinusmp <shrinivaspatil12@gmail.com>
2022-06-25 13:00:46 +0530

    UMAP-8:created binding class of user

---
+++ src/main/java/com/BikkadIt/UMA/binding/UserDTO.java
@@ -0,0 +1,34 @@
+package com.BikkadIt.UMA.binding;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class UserDTO {
+	
+private Integer userId;
+	
+	@NotEmpty
+	@Size(min = 4 ,max = 10,message = "Firstname shoudbe minium 3 chars and amximum 10 chars")
+	private String userFirstName;
+	
+	@NotEmpty
+	@Size(min = 4 ,max = 10,message = "lastname shoudbe minium 3 chars and amximum 10 chars")
+	private String userLastName;
+	
+	@Email
+	private String userEmail;
+	@NotEmpty
+	private String userPhNo;
+	@NotEmpty
+	private String userDOB;
+	@NotEmpty
+	private String gender;
+	@NotEmpty
+	private String userCountry;
+	@NotEmpty
+	private String userState;
+	@NotEmpty
+	private String userCity;
+
+}
